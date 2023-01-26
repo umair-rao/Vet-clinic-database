ced496610b64862ff38de9e71d06120721b4a6ad
Umair Muqeem <umairmuqeem@gmail.com>
2023-01-26 16:01:59 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+vet_clinic=# CREATE TABLE animals (
+vet_clinic(# id BIGSERIAL NOT NULL PRIMARY KEY,
+vet_clinic(# name CHAR(20) NOT NULL,
+vet_clinic(# date_of_birth DATE NOT NULL,
+vet_clinic(# escape_attempts INT,
+vet_clinic(# neutered BOOLEAN,
+vet_clinic(# weight_kg DECIMAL );
