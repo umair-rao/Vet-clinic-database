8dfec0ddd0525b0d753c0a13f25656b725cbb0e7
Peter chendi <petzyrockchendi@gmail.com>
2023-02-01 10:58:21 +0100

    create the medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,16 @@ CREATE TABLE patients (
     date_of_birth  date
 );
 
+--Create the medical_histories table
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+    admitted_at timestamp NOT NULL,
+    patient_id int,
+    status varchar(100),
+    FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
+
+--create the index for the patient_id in ascending order to reduce query execution time
+CREATE INDEX patients_id_asc ON medical_histories (patient_id ASC);
+
+
